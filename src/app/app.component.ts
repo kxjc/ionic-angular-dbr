56e7edee5cd0b5a5934058d5f98f11f0dff31733
kxjc <61426371+kxjc@users.noreply.github.com>
2020-04-02 21:28:29 -0700

    ionic x dbr sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import Dynamsoft from 'dynamsoft-javascript-barcode';
 
 @Component({
   selector: 'app-root',
@@ -23,5 +24,8 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@7.3.0-v2/dist/";
+    // Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license. 
+    Dynamsoft.BarcodeReader.productKeys = "t0068MgAAADcOB47dibaMd0Hpe8N+9G3AcVoUNfCkl4V3cAh0TXhsmbiJewPxP0eOBnp0QzjNmMitkEn+Iz6Phm5rvC5YVp0=";
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    ɵCompiler_compileModuleSync__POST_R3__
+} from "@angular/core";
+import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
+import { Platform, ToastController } from "@ionic/angular";
+
+import Dynamsoft from "dynamsoft-javascript-barcode";
+import { timingSafeEqual } from "crypto";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: "app-home",
+    templateUrl: "home.page.html",
+    styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+    private platform: Platform;
+    public toastMsgs: any = [];
 
-  constructor() {}
+    constructor(platform: Platform, public toastController: ToastController) {
+        this.platform = platform;
+    }
 
+    reader = null;
+    scanner = null;
+
+    async readBarcodeFromImage() {
+        try {
+            this.reader =
+                this.reader || (await Dynamsoft.BarcodeReader.createInstance());
+            const image = await Plugins.Camera.getPhoto({
+                quality: 100,
+                allowEditing: false,
+                resultType: CameraResultType.DataUrl,
+                source: CameraSource.Camera
+            });
+            let results = await this.reader.decode(image.dataUrl);
+            for (let result of results) {
+                this.sendToast(result.barcodeText);
+            }
+        } catch (ex) {
+            if (!ex.includes("User cancelled photos app")) {
+                alert(ex);
+            }
+        }
+    }
+
+    async readBarcodeFromVideo() {
+        try {
+            this.scanner =
+                this.scanner ||
+                (await Dynamsoft.BarcodeScanner.createInstance());
+            let scanSettings = await this.scanner.getScanSettings();
+            // disregard duplicated results found in a specified time period
+            scanSettings.duplicateForgetTime = 1000;
+            // set a scan interval so the library may release the CPU from time to time
+            scanSettings.intervalTime = 2000;
+            await this.scanner.updateScanSettings(scanSettings);
+
+            this.scanner.onUnduplicatedRead = (txt, result) => {
+                this.sendToast(txt);
+            };
+
+            await this.scanner.show();
+            document.getElementsByClassName(
+                "dbrScanner-cvs-scanarea"
+            )[0].parentElement.style.height = "90%";
+        } catch (ex) {
+            alert(ex);
+        }
+    }
+
+    async sendToast(barcodeText) {
+        this.toastMsgs.push(barcodeText);
+        const toast = await this.toastController.create({
+            message: this.toastMsgs
+                .toString()
+                .split(",")
+                .join("\n"),
+            duration: 2000
+        });
+        toast.present();
+        toast.onDidDismiss().then(() => {
+            this.toastMsgs = [];
+        });
+    }
 }
